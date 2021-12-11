aed210cc8c7e13f039c9572d61b370cd9ec3c2d0
Jon-Felipe <rodriguesjf7@gmail.com>
2021-12-11 21:46:17 +0200

    Serving & Fetching Data From Express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
