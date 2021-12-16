77a5dd4a45ab4193b9e5e50c81c8654383ebca18
Jon-Felipe <rodriguesjf7@gmail.com>
2021-12-16 13:26:55 +0200

    Update profile endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart">
               <Route path=":id" element={<CartScreen />} />
