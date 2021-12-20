a82ee4e79c1b42ed52a151400273957585f9e1cc
Jon-Felipe <rodriguesjf7@gmail.com>
2021-12-20 16:50:54 +0200

    Payment screen & save payment Method & Checkout steps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -6,6 +6,7 @@ import { saveShippingAddress } from '../actions/cartActions';
 
 // components
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingScreen = () => {
   const navigate = useNavigate();
@@ -27,6 +28,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
