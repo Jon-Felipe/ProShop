fbe6f5944e78868021ae70c1c9cb5d568e104f8d
Jon-Felipe <rodriguesjf7@gmail.com>
2021-12-21 10:13:17 +0200

    Create order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,13 +7,13 @@ import Order from '../models/orderModel.js';
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
-    shippingAdderss,
+    shippingAddress,
     paymentMethod,
     itemsPrice,
     taxPrice,
     shippingPrice,
     totalPrice,
-  } = res.body;
+  } = req.body;
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
@@ -23,13 +23,14 @@ const addOrderItems = asyncHandler(async (req, res) => {
     const order = new Order({
       orderItems,
       user: req.user._id,
-      shippingAdderss,
+      shippingAddress,
       paymentMethod,
       itemsPrice,
       taxPrice,
       shippingPrice,
       totalPrice,
     });
+
     const createdOrder = await order.save();
 
     res.status(201).json(createdOrder);
